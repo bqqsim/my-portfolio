f0b0f30583747054f3b91db18d593696d1d74afb
bqqsim <747034@pdsb.net>
2020-10-23 19:16:37 -0400

    new-update (js) 10-23

--- script/main.js
+++ script/main.js
@@ -10,3 +10,9 @@ function greeting() {
  let name = prompt("whats your name");
  alert("Hello "+ name);
 }
+/*
+function varDemo(){
+let firstNumber = 9;
+let secondNumber = 85;
+let answer = firstNumber + secondNumber;
+}*/

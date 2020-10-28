d33131e322fdec54c91c858b96ee859ecf69f69e
bqqsim <747034@pdsb.net>
2020-10-28 01:01:52 -0400

    new update (buttons JS) (10-28)

--- script/main.js
+++ script/main.js
@@ -1,14 +1,53 @@
-function sayHey() {
-    alert("SURPRISE HAHA");
+function button1() {
+    let name = prompt ('Hey there! What\'s your name?')
+    prompt("What brings you here " + name);
+    alert('That\'s cool! Enjoy the rest of your time here!');
 }
 
-function sayBye() {
-    alert("BYE");
+function button2() {
+    let name = prompt("How has your time on this website been?");
+    alert("Well that\'s "+ name + ('!'));
 }
 
-function greeting() {
- let name = prompt("whats your name");
- alert("Hello "+ name);
+function button3() {
+    prompt('Are you just looking through these buttons? Answer with Yes or No!')
+    if ("Yes") {
+    alert ('Oh, That\'s nice. You may continue');}
+    else ("No"); 
+    
+}
+function button4() {
+    location.reload();
+alert ('Boom. Press OK and the large text will disappear!')
+}
+
+function button5() {
+    prompt ('Aren\'t you getting bored of the same old pop ups?');
+    alert ('Let\'s see what is in the other buttons.')
+}
+
+function button6() {
+    alert ('Well, by the looks of it, you are not bored of these yet!')
+}
+
+function button7 () {
+    alert ('Soon, these buttons will do more than these lame alerts! Keep checking in from time to time!')
+}
+
+function button8 () {
+    alert ('boop. There\'s nothing special here!! Sorry!')
+}
+
+function button9 () {
+    alert ('Yes, nine buttons. There may be less in the final product for this website!')
+}
+
+function button10 () {
+    alert ('Woah 10 buttons! Still nothing cool though :(')
+}
+
+function button11 () {
+    alert ('This is kind of disappointing considering how interactive the other buttons were right? Dang :(')
 }
 /*
 function varDemo(){
